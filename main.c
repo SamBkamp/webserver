f6ec8492fddc1361ea1ee77191e534f74d6aea94
Sam Bonnekamp <sam@bonnekamp.net>
2025-10-30 17:41:45 +0800

    moved error logging out

--- main.c
+++ main.c
@@ -18,15 +18,13 @@ int open_connection(int *sockfd){
   struct sockaddr_in host_addr;
   //init socket
   *sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if(*sockfd < 0){
-    perror("socket");
+  if(*sockfd < 0)
     return -1;
-  }
 
   //socket options
   if(setsockopt(*sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) != 0)
     perror("[NON-FATAL] couldn't set sockopt\n");
-  
+
   //init address
   memset(&host_addr, 0, sizeof(struct sockaddr_in));
   host_addr.sin_family = AF_INET;
@@ -34,16 +32,13 @@ int open_connection(int *sockfd){
   host_addr.sin_addr = (struct in_addr){INADDR_ANY};
 
   //bind address
-  if(bind(*sockfd, (struct sockaddr *)&host_addr, sizeof(host_addr))!=0){
-    perror("bind");
-    return -1;    
-  }
+  if(bind(*sockfd, (struct sockaddr *)&host_addr, sizeof(host_addr))!=0)
+    return -1;
+
 
   //set socket to listening
-  if(listen(*sockfd, QUEUE_LEN) != 0){
-    perror("listen");
+  if(listen(*sockfd, QUEUE_LEN) != 0)
     return -1;
-  }
   return 0;
 }
 
@@ -52,8 +47,9 @@ int main(int argc, char* argv[]){
   struct sockaddr_in peer_addr;
   socklen_t peer_size = sizeof(struct sockaddr_in);
   char buffer[1024];
-  
+
   if(open_connection(&sockfd) != 0){
+    perror("open_connection");
     return 1;
   }
   while(1){
@@ -62,7 +58,7 @@ int main(int argc, char* argv[]){
     ssize_t bytes_read = read(peer_fd, buffer, 1023);
     buffer[bytes_read] = 0;
     printf("%s\n", buffer);
-    
+
     close(peer_fd);
   }
 }
