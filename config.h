691a993e25c7f1827d78d22d9b236cd38e8bab4f
Sam Bonnekamp <sam@bonnekamp.net>
2025-11-05 17:16:48 +0800

    added config file

---
+++ config.h
@@ -0,0 +1,10 @@
+//some config info for easy incorporations. Will be replaced with a proper on-disk runtime config.. eventually...
+#ifndef PWS_CONFIG
+#define PWS_CONFIG
+
+#define PRIVATE_KEY_FILE "certs/priv.pem"
+#define CERTIFICATE_FILE "certs/cert.pem"
+
+#define HOST_NAME "fish"
+
+#endif
